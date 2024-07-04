889d273ec0b44477cf4eadfb9c7d547b548145c8
IsraelMerlyn <merlyn142a@gmail.com>
2024-07-03 18:25:49 -0600

    tasks #0.4.1 Implementando metodos de servicios

--- src/main/java/com/israelmerlyn/sistemas_rh/services/IEmpleadoServicio.java
+++ src/main/java/com/israelmerlyn/sistemas_rh/services/IEmpleadoServicio.java
@@ -5,5 +5,10 @@ import com.israelmerlyn.sistemas_rh.model.Empleado;
 import java.util.List;
 
 public interface IEmpleadoServicio {
+    public List<Empleado> listarEmpleados();
+    public Empleado buscarEmpleadoPorId(Integer idEmpleado);
+    public Empleado guardarEmpleado(Empleado empleado);
+    public Empleado actualizarEmpleado(Empleado empleado);
+    public void  eliminarEmpleado(Empleado empleado);
 
 }
