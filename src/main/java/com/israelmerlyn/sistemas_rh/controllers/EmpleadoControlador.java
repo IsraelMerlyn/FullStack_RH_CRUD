9f5eaf781878647c12732f7bff418939f553b20a
IsraelMerlyn <merlyn142a@gmail.com>
2024-07-04 19:46:35 -0600

    tasks #0.5.2 Implementando Metodo ListarEmpleados

--- src/main/java/com/israelmerlyn/sistemas_rh/controllers/EmpleadoControlador.java
+++ src/main/java/com/israelmerlyn/sistemas_rh/controllers/EmpleadoControlador.java
@@ -6,9 +6,9 @@ import com.israelmerlyn.sistemas_rh.services.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rh-app")
@@ -17,8 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class EmpleadoControlador {
     private  static  final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+
     @Autowired
     private IEmpleadoServicio empleadoServicio;
+
+    //http://localhost:8080/rh-app/empleados
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados() {
+        var empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach(empleado -> logger.info(empleado.toString()));
+        return empleados;
+    }
+
+    @PutMapping
+    public Empleado guardarEmpleado(@RequestBody Empleado empleado) {
+        return empleadoServicio.guardarEmpleado(empleado);
+    }
 
 
 }
