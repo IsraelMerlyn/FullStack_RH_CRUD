65d22f43024b450411e0a760fa2d5a0581e659d4
IsraelMerlyn <merlyn142a@gmail.com>
2024-07-04 19:52:57 -0600

    tasks #0.6 Cambiando Estructura del Proyecto de BackEnd y FrontEnd

--- src/main/java/com/israelmerlyn/sistemas_rh/repository/EmpleadoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.israelmerlyn.sistemas_rh.repository;
-
-import com.israelmerlyn.sistemas_rh.model.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadoRepositorio extends JpaRepository <Empleado, Integer> {
-}
