e0e0c32de62df865a18e5700ef61e516f1f80190
IsraelMerlyn <merlyn142a@gmail.com>
2024-07-09 11:22:51 -0600

    tasks #9.1 Agregando Barra de Navegacion

--- sistema_rh_frontend/src/plantilla/Navegacion.jsx
+++ sistema_rh_frontend/src/plantilla/Navegacion.jsx
@@ -1,6 +1,24 @@
-export default funtion Nevegacion(){
-    return (
-        <>
-        </>
-        )
-    }
+import React from 'react'
+
+export default function Navegacion() {
+  return (
+    <>
+    <div classNameName="container">
+
+    <ul className="nav justify-content-end navbar-dark bg-primary">
+  <li className="nav-item">
+    <a className="nav-link active text-black" aria-current="page"  href="/">Inicio</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link text-black" href="/agregar">Agregar</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link text-black" href="/listar">Listar</a>
+  </li>
+ 
+</ul>
+    </div>
+    
+    </>
+  )
+}
